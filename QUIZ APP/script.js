99afdf2920a712b4f8400e62740af89c433aa78b
Pradumn Dubey <140749011+dubeypradum999@users.noreply.github.com>
2023-08-25 16:26:40 +0530

    Add files via upload

---
+++ QUIZ APP/script.js
@@ -0,0 +1,121 @@
+var quiz = {
+  // (A) PROPERTIES
+  // (A1) QUESTIONS & ANSWERS
+  // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+  data: [
+    {
+      q: "Number of primitive data types in Java are?",
+      o: ["4", "7", "8", "12"],
+      a: 2, // arrays start with 0, so answer is 70 meters
+    },
+    {
+      q: "What is the size of float and double in java?",
+      o: ["32 and 64", "24 and 48", "32 and 32", "64 and 64"],
+      a: 0,
+    },
+    {
+      q: "Automatic type conversion is possible in which of the possible cases?",
+      o: ["Byte to Int", "Int to Long", "Long to Int", "Short to Int"],
+      a: 1,
+    },
+    {
+      q: "Which is the seventh planet from the sun?",
+      o: ["Uranus", "Earth", "Pluto", "Mars"],
+      a: 0,
+    },
+    {
+      q: " When an array is passed to a method, what does the method receive ?",
+      o: ["The Refrence of Array", "Copy of Array", "Length of Array", "Copy of First Element "],
+      a: 0,
+    },
+  ],
+
+  // (A2) HTML ELEMENTS
+  hWrap: null, // HTML quiz container
+  hQn: null, // HTML question wrapper
+  hAns: null, // HTML answers wrapper
+
+  // (A3) GAME FLAGS
+  now: 0, // current question
+  score: 0, // current score
+
+  // (B) INIT QUIZ HTML
+  init: () => {
+    // (B1) WRAPPER
+    quiz.hWrap = document.getElementById("quizWrap");
+
+    // (B2) QUESTIONS SECTION
+    quiz.hQn = document.createElement("div");
+    quiz.hQn.id = "quizQn";
+    quiz.hWrap.appendChild(quiz.hQn);
+
+    // (B3) ANSWERS SECTION
+    quiz.hAns = document.createElement("div");
+    quiz.hAns.id = "quizAns";
+    quiz.hWrap.appendChild(quiz.hAns);
+
+    // (B4) GO!
+    quiz.draw();
+  },
+
+  // (C) DRAW QUESTION
+  draw: () => {
+    // (C1) QUESTION
+    quiz.hQn.innerHTML = quiz.data[quiz.now].q;
+
+    // (C2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    for (let i in quiz.data[quiz.now].o) {
+      let radio = document.createElement("input");
+      radio.type = "radio";
+      radio.name = "quiz";
+      radio.id = "quizo" + i;
+      quiz.hAns.appendChild(radio);
+      let label = document.createElement("label");
+      label.innerHTML = quiz.data[quiz.now].o[i];
+      label.setAttribute("for", "quizo" + i);
+      label.dataset.idx = i;
+      label.addEventListener("click", () => {
+        quiz.select(label);
+      });
+      quiz.hAns.appendChild(label);
+    }
+  },
+
+  // (D) OPTION SELECTED
+  select: (option) => {
+    // (D1) DETACH ALL ONCLICK
+    let all = quiz.hAns.getElementsByTagName("label");
+    for (let label of all) {
+      label.removeEventListener("click", quiz.select);
+    }
+
+    // (D2) CHECK IF CORRECT
+    let correct = option.dataset.idx == quiz.data[quiz.now].a;
+    if (correct) {
+      quiz.score++;
+      option.classList.add("correct");
+    } else {
+      option.classList.add("wrong");
+    }
+
+    // (D3) NEXT QUESTION OR END GAME
+    quiz.now++;
+    setTimeout(() => {
+      if (quiz.now < quiz.data.length) {
+        quiz.draw();
+      } else {
+        quiz.hQn.innerHTML = `You have answered ${quiz.score} of ${quiz.data.length} correctly.`;
+        quiz.hAns.innerHTML = "";
+      }
+    }, 1000);
+  },
+
+  // (E) RESTART QUIZ
+  reset: () => {
+    quiz.now = 0;
+    quiz.score = 0;
+    quiz.draw();
+  },
+};
+window.addEventListener("load", quiz.init);
